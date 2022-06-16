850a777c1c7bbd3b3d9e53554d5b0ff7f186ddc2
martinadero <mdaireaux@udesa.edu.ar>
2022-06-16 01:38:10 -0300

    arreglos

--- js/generos.js
+++ js/generos.js
@@ -1,29 +1,20 @@
-// Consultar la api para recuperar la información
-// usamos fetch
 let proxy = "https://api.allorigins.win/raw?url="
 let endpoint = "https://api.deezer.com/genre"
 
-fetch (`${proxy}${endpoint}`) // fecth(proxy+endpoint)
-.then(function(data){
-    // hacer nuestro codigo
-    // lista de generos
-    // imagen
-    // nombre del genero
-    // redirigir el detalle del genero
+fetch(`${proxy}${endpoint}`) // fetch(proxy+endpoint)
+.then(function(data) {
+    return data.json()
+})
+.then(function(data) {
     let ul = document.querySelector("ul")
-    for (let i = 0; i < data.data.length; i++){
-         ul.innerHTML += ` 
-         <li>
-              <img scr = "${data.data[i].picture_xl}" >
-              <a href= "generodetail.html?q=${data.data[i].id}">${data.data[i].name} </a>
-          </li>
-          `
-
+    for (let i = 0; i < data.data.length; i++) {
+        ul.innerHTML += `<li>
+                            <img src="${data.data[i].picture_xl}" >
+                            <a href="generodetail.html?q=${data.data[i].id}">${data.data[i].name}</a>
+                        </li>`
     }
-
- })
- .catch(function(error) {console.log(error)})
-
+})
+.catch(function(error) {console.log(error)})
 
 
 
